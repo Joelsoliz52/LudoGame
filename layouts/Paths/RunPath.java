24c7e1f448b65618ddb2a5013053ea2148d56a7b
MayteMejia <80721079+MayteMejia@users.noreply.github.com>
2021-06-12 20:03:09 -0400

    Add files via upload

--- layouts/Paths/RunPath.java
+++ layouts/Paths/RunPath.java
@@ -3,17 +3,19 @@ package layouts.Paths;
 import java.awt.*;
 
 import interfaces.Path;
+
 public class RunPath implements Path {
     private int[][] AX;
     private int[][] AY;
     private int tam;
     public RunPath(int tam, Color[] colores){
         this.tam = tam;
-        addAX(AX, colores);
-        addAY(AY, colores);
+        addAX(colores);
+        addAY(colores);
         getAX();
         getAY();
     }
+
     /**
      * Returns possible X positions of the pawns.
      * @return Possible X positions.
@@ -21,14 +23,14 @@ public class RunPath implements Path {
     public int[][] getAX() {
         return AX;
     }
-    
-    private void addAX(int[][] x, Color[] colores){
+
+    private void addAX(Color[] colores){
         int i = 0;
         int pos = 0;
         AX = new int[4][84];
-        
+
         while(pos < tam){
-            if(colores[pos] == Color.BLUE){                   
+            if(colores[pos] == Color.BLUE){
                 int[] a1 = {3,4,4,4,4,5,6,7,7,7,7,7,7,8,9,10,10,10,10,10,10,11,12,13,14,15,16,17,18,18,18,18,18,18,18,18,18,17,16,15,14,13,12,11,11,11,11,11,11,10,9,8,8,8,8,8,8,7,6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,1,2,3,4,4,4,4,5,6,7,8};
                 while(i < 84){
                     AX[pos][i] = a1[i];
@@ -37,8 +39,8 @@ public class RunPath implements Path {
                 i = 0;
             }
             if(colores[pos] == Color.GREEN){
-                int[] a2 = {15,14,13,13,13,13,13,13,14,15,16,17,18,18,18,18,18,18,18,18,18,17,16,15,14,13,12,11,11,11,11,11,11,10,9,8,8,8,8,8,8,7,6,5,4,3,2,1,0,0,0,0, 0, 0,0,0,1,2,3,4,5,6,7,7,7,7,7,7,8,9,10,10,10,10,10,10,10,10,10};
-                while(i < 79){
+                int[] a2 = {15,14,13,13,13,13,13,13,14,15,16,17,18,18,18,18,18,18,18,18,18,17,16,15,14,13,12,11,11,11,11,11,11,10,9,8,8,8,8,8,8,7,6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,7,7,7,7,7,8,9,10,10,10,10,10,10,10,10,10};
+                while(i < 80){
                     AX[pos][i] = a2[i];
                     i++;
                 }
@@ -70,8 +72,8 @@ public class RunPath implements Path {
     public int[][] getAY() {
         return AY;
     }
-    
-    private void addAY(int[][] y, Color[] colores){
+
+    private void addAY(Color[] colores){
         int pos = 0;
         int i = 0;
         AY = new int[4][84];
@@ -86,7 +88,7 @@ public class RunPath implements Path {
             }
             if(colores[pos] == Color.GREEN){
                 int[] a2 = {0,0,0,1,2,3,4,5,5,5,5,5,5,6,7,8,9,10,11,12,13,13,13,13,13,13,13,13,14,15,16,17,18,18,18,18,17,16,15,14,13,13,13,13,13,13,13,13,13,12,11,10,9,8,7,6,5,5,5,5,5,5,5,5,4,3,2,1,0,0,0,0,1,2,3,4,5,6,7,8};
-                while(i < 79){
+                while(i < 80){
                     AY[pos][i] = a2[i];
                     i++;
                 }
@@ -102,7 +104,7 @@ public class RunPath implements Path {
             }
             if(colores[pos] == Color.YELLOW){
                 int[] a4 = {18,18,18,17,16,15,14,13,13,13,13,13,13,12,11,10,9,8,7,6,5,5,5,5,5,5,5,5,4,3,2,1,0,0,0,0,1,2,3,4,5,5,5,5,5,5,5,5,5,6,7,8,9,10,11,12,13,13,13,13,13,13,13,13,14,15,16,17,18,18,18,18,17,16,15,14,13,12,11,10};
-    
+
                 while(i < 80){
                     AY[pos][i] = a4[i];
                     i++;
@@ -112,16 +114,23 @@ public class RunPath implements Path {
             pos++;
         }
     }
-    
+
     /**
      * Returns optional X positions of the pawns.
-     * @return null.
+     * @return Possible X positions.
      */
-    public int[][] getOptionalAX() { return null; }
+    public int[][] getOptionalAX() {
+        return new int[][]{
+                {1,2,3,4,5,6,7,7,7,7,7,7,8,9,10,10,10,10,10,10,11,12,13,14,15,16,17,18,18,18,18,18,18,18,18,18,17,16,15,14,13,12,11,11,11,11,11,11,10,9,8,8,8,8,8,8,7,6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7}
+        };
+    }
 
     /**
      * Returns possible X positions of the pawns.
-     * @return null.
+     * @return Possible X positions.
      */
-    public int[][] getOptionalAY() { return null; }
-}
+    public int[][] getOptionalAY() {
+        return new int[][]{
+                {5,5,5,5,5,5,5,4,3,2,1,0,0,0,0,1,2,3,4,5,5,5,5,5,5,5,5,5,6,7,8,9,10,11,12,13,13,13,13,13,13,13,13,14,15,16,17,18,18,18,18,17,16,15,14,13,13,13,13,13,13,13,13,13,12,11,10,9,8,7,6,5,5,5,5,5,5,5,5}
+        };
+    }}

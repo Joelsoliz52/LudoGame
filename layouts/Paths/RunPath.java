b7e14b69b305173ba6dd9714486019a9d55fcb52
MayteMejia <80721079+MayteMejia@users.noreply.github.com>
2021-07-07 19:09:49 -0400

    Add files via upload

--- layouts/Paths/RunPath.java
+++ layouts/Paths/RunPath.java
@@ -67,7 +67,7 @@ public class RunPath implements Path, Serializable {
                 i = 0;
             }
             if (colores[pos].equals(Color.YELLOW)) {
-                int[] a4 = { 3, 4, 4, 4, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7,
+                int[] a4 = { 3, 4, 4, 4, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7,
                         8, 9, 10, 10, 10, 10, 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18,
                         17, 16, 15, 14, 13, 12, 11, 11, 11, 11, 11, 11, 10, 9, 8, 8, 8, 8, 8, 8, 7, 6, 5, 4, 4, 4, 4, 5,
                         6, 7, 8 };
@@ -145,8 +145,7 @@ public class RunPath implements Path, Serializable {
     }
 
     /**
-     * Returns optional X positions of the pawns.
-     * 
+     * Returns optional X positions of the pawns
      * @return Possible X positions.
      */
     public int[][] getOptionalAX() {
@@ -157,7 +156,6 @@ public class RunPath implements Path, Serializable {
 
     /**
      * Returns possible X positions of the pawns.
-     * 
      * @return Possible X positions.
      */
     public int[][] getOptionalAY() {
